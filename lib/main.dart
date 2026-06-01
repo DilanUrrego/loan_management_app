c7944b3248bd28a285505d6123d222bee6a08c49
Dilan <durrego124@gmail.com>
2026-05-31 20:22:21 -0500

    Andorid Firebase Configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'pages/home_page.dart';
+import 'firebase_options.dart';
 
 
+/*
 void main() {
   runApp(const MyApp());
 }
-
+*/
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -20,4 +24,22 @@ class MyApp extends StatelessWidget {
       home: const HomePage(),
     );
   }
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+
+  // Prueba rápida de "Crear" (Create)
+  try {
+    await FirebaseFirestore.instance.collection('pruebas').add({
+      'mensaje': '¡Conexión exitosa desde Flutter!',
+      'fecha': DateTime.now(),
+    });
+    print("✅ ¡Datos enviados a Firebase correctamente!");
+  } catch (e) {
+    print("❌ Error al enviar datos: $e");
+  }
+
+  runApp(const MyApp());
 }
